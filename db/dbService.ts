1e1748e7d867635185a7e72e1d56e028525e605b
stachkrol <st.krolikiewicz@gmail.com>
2023-01-13 19:12:05 +0100

    create CRUD endpoints for books in REST API style

--- db/dbService.ts
+++ db/dbService.ts
@@ -1,6 +1,9 @@
 import mysql, { Connection, MysqlError } from "mysql";
 import dotenv from "dotenv";
 import { createSchemas } from "./createSchemas";
+import { Request, Response } from "express";
+
+let instance: DbService | null = null;
 
 dotenv.config();
 
@@ -8,7 +11,6 @@ const connection: Connection = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    // database: process.env.DATABASE,
 });
 
 connection.connect((err: MysqlError) => {
@@ -25,3 +27,101 @@ connection.connect((err: MysqlError) => {
     }
     console.log("[dbService] Database " + connection.state);
 });
+
+export class DbService {
+    static getDbServiceInstance(): DbService {
+        return instance ? instance : new DbService();
+    }
+
+    async insertNewBook(req: Request): Promise<any> {
+        try {
+            const { name, year, price, pages, description, image } = req.body;
+
+            const response = await new Promise((resolve, reject) => {
+                const query = `INSERT INTO books (name, year, price, pages, description, image) VALUES ("${name}", "${year}", ${price}, ${pages} ,"${description}", "${image}");`;
+
+                connection.query(query, (err, result) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(result);
+                    console.log(
+                        `[dbService] ${result.affectedRows} row inserted into books table`
+                    );
+                });
+            });
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    async getAllData(): Promise<any> {
+        try {
+            const response = await new Promise((resolve, reject) => {
+                const query = `SELECT * FROM books;`;
+
+                connection.query(query, (err, results) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(results);
+                });
+            });
+            return response;
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
+    async updateBook(req: Request): Promise<any> {
+        try {
+            const {
+                id,
+                name,
+                year,
+                price,
+                pages,
+                description,
+                image,
+            } = req.body;
+
+            const response = await new Promise((resolve, reject) => {
+                const query = `UPDATE books SET name = "${name}", year = ${year}, price = ${price}, pages = ${pages}, description = "${description}", image = "${image}" WHERE id = ${parseInt(
+                    id,
+                    10
+                )};`;
+
+                connection.query(query, (err, result) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(result.affectedRows);
+                    console.log(
+                        `[dbService] ${result.affectedRows} row updated in books table`
+                    );
+                });
+            });
+            return response === 1 ? true : false;
+        } catch (err) {
+            console.log(err);
+            return false;
+        }
+    }
+
+    async deleteBook(id: string): Promise<any> {
+        try {
+            const response = await new Promise((resolve, reject) => {
+                const query = `DELETE FROM books WHERE id = ${parseInt(
+                    id,
+                    10
+                )};`;
+
+                connection.query(query, (err, result) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(result.affectedRows);
+                    console.log(
+                        ` [dbService] ${result.affectedRows} row deleted from books table`
+                    );
+                });
+            });
+            return response === 1 ? true : false;
+        } catch (err) {
+            console.log(err);
+            return false;
+        }
+    }
+}
--- server.ts
+++ server.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
+import { DbService } from "./db/dbService";
 const dbService = require("./db/dbService");
 
 dotenv.config();
@@ -9,9 +10,49 @@ const app: Express = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
+//create
+app.post("/insert", (req: Request, res: Response) => {
+    const db = DbService.getDbServiceInstance();
+    const result = db.insertNewBook(req);
+
+    result
+        .then((data: any) => res.json({ data: data }))
+        .catch((err: Error) => console.log(err));
+});
+
+//read
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World!");
+    const db = DbService.getDbServiceInstance();
+
+    const result = db.getAllData();
+
+    result
+        .then((data: any) => res.json({ data: data }))
+        .catch((err: Error) => console.log(err));
+});
+
+//update
+app.patch("/update", (req: Request, res: Response) => {
+    const db = DbService.getDbServiceInstance();
+
+    const result = db.updateBook(req);
+
+    result
+        .then((data: any) => res.json({ success: data }))
+        .catch((err: Error) => console.log(err));
+});
+
+//delete
+app.delete("/delete/:id", (req: Request, res: Response) => {
+    const db = DbService.getDbServiceInstance();
+
+    const result = db.deleteBook(req.params.id);
+
+    result
+        .then((data: any) => res.json({ success: data }))
+        .catch((err: Error) => console.log(err));
 });
 
 app.listen(port, () => {
